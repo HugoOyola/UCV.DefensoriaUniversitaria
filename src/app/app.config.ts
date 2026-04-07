2ce62806c9f038d71ff0509326f29dd7bb28f7d3
Hugo Ham <hugo.ham@outlook.com>
2026-04-07 16:50:27 -0500

    Agregar PrimeNG y PrimeUIX como dependencias y configurar el tema en la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
-
-import {routes} from './app.routes';
-import {provideAnimations} from '@angular/platform-browser/animations';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {tokenInterceptor} from '@auth/token.interceptor';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideClientHydration} from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config'
+import Aura from '@primeuix/themes/aura';
+import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from '@auth/token.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -14,5 +15,13 @@ export const appConfig: ApplicationConfig = {
 		provideAnimations(),
 		provideHttpClient(withInterceptors([tokenInterceptor])),
 		provideAnimationsAsync(),
+		providePrimeNG({
+			theme: {
+				preset: Aura,
+				options: {
+					darkModeSelector: false,
+				},
+			},
+		})
 	],
 };
